768ed62da74449f3d648204ce7845f158fccc1b9
Administrator <Administrator@WIN-091B0VJKTNC>
2017-06-10 11:44:48 +0800

    添加项目文件。

---
+++ MvcMusicStore_Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcMusicStore_Web.Startup))]
+namespace MvcMusicStore_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
