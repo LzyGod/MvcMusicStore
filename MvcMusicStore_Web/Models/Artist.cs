9e6d869bcb6a06a52057242fe1df0f4ed6ccb2af
LayGod <274287759@qq.com>
2017-07-10 19:04:22 +0800

    添加Models

---
+++ MvcMusicStore_Web/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcMusicStore_Web.Models
+{
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
+    }
+}
